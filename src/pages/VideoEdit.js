8c9aa672c97381614c3b25a23120478690b5ba6d
Jung Young Jun <dudwns0921@gmail.com>
2022-05-06 00:52:27 +0900

    Customizing webpack configuration & Add Changing title feature

--- src/pages/VideoEdit.js
+++ src/pages/VideoEdit.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import { useState, React } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import videos from '../dummy/DummyVideos';
 
 function VideoEdit() {
+  const [newTitle, setNewTitle] = useState(' ');
   let { id } = useParams();
   let navigate = useNavigate();
   const videoObj = videos[id - 1];
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('post new title!');
-    navigate('/');
+    videos[id - 1].title = newTitle;
+    navigate(`/video/${id}`);
+  };
+
+  const handleTitleChange = (e) => {
+    setNewTitle(e.target.value);
   };
   return (
     <div>
       <h1>Editing : {videoObj.title}</h1>
-      <form onSubmit={handleSubmit}>
-        <input placeholder="Video Title" type="text" />
+      <form onSubmit={handleSubmit} action="post">
+        <input
+          placeholder="Video Title"
+          type="text"
+          onChange={handleTitleChange}
+        />
         <input type="submit" value="save" />
       </form>
     </div>
